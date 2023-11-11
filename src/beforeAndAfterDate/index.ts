490b57de7b9c18abd7290745c638dc7ef4508ea1
Fredrik Johansen <fredrik1206@gmail.com>
2023-11-11 21:42:27 +0100

    fix(beforeAndAfter): adds return and fixes return types

--- src/beforeAndAfterDate/index.ts
+++ src/beforeAndAfterDate/index.ts
@@ -3,10 +3,15 @@ interface BeforeAndAfterDateProps {
   date: Date;
 }
 
+interface BeforeAndAfterDateReturns {
+  beforeDate: Date | null;
+  afterDate: Date | null;
+}
+
 export const beforeAndAfterDate = ({
   dates,
   date,
-}: BeforeAndAfterDateProps) => {
+}: BeforeAndAfterDateProps): BeforeAndAfterDateReturns => {
   const datesInUnix = dates.map((date) => date.getTime());
   const dateInUnix = date.getTime();
 
@@ -17,6 +22,14 @@ export const beforeAndAfterDate = ({
   const sortedDatesUnix = datesInUnix.sort();
   const specifiedDateIndex = sortedDatesUnix.indexOf(dateInUnix); // We can ensure that the date is there, because we add it, if it's not present
 
-  const beforeDate = datesInUnix[specifiedDateIndex - 1] || undefined;
-  const afterDate = datesInUnix[specifiedDateIndex - 1] || undefined;
+  const beforeDateUnix = datesInUnix[specifiedDateIndex - 1];
+  const afterDateUnix = datesInUnix[specifiedDateIndex + 1];
+
+  const beforeDate = beforeDateUnix ? new Date(beforeDateUnix) : null;
+  const afterDate = afterDateUnix ? new Date(afterDateUnix) : null;
+
+  return {
+    beforeDate,
+    afterDate,
+  };
 };
